9117c2cefd2635161fbfd045a81026eec205b8d6
Molly Miller <55203910+mollym55@users.noreply.github.com>
2022-02-20 20:15:43 -0600

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -3,28 +3,28 @@ import copy
 
 # The Transfer class represents the singleton transfer operation
 class Transfer:
-    def __init__(self, state, recieved, to, trade):
+    def __init__(self, state, recieve, deliver, trade):
         self.state = copy.deepcopy(state)
-        self.recieved = recieved
-        self.country_to = country_to
+        self.recieve = recieve
+        self.deliver = deliver
         self.trade = trade
 
     def execute(self):
         resource = self.trade[0]
         quantity = self.trade[1]
-        self.state[self.country_from][resource] = self.state[self.country_from][resource] - quantity
-        self.state[self.country_to][resource] = self.state[self.country_to][resource] + quantity
+        self.state[self.recieve][resource] = self.state[self.recieve][resource] - quantity
+        self.state[self.deliver][resource] = self.state[self.deliver][resource] + quantity
         return self.state
 
     def toString(self):
         resource = self.trade[0]
         quantity = self.trade[1]
-        string = "(TRANSFER FROM " + self.country_from + " TO " + \
-            self.country_to + " (" + resource + " " + str(quantity) + "))"
+        string = "(TRANSFER FROM " + self.recieve + " TO " + \
+            self.deliver + " (" + resource + " " + str(quantity) + "))"
         return string
 
     def isFrom(self):
-        return self.country_from
+        return self.recieve
 
     def isTo(self):
-        return self.country_to
+        return self.deliver
