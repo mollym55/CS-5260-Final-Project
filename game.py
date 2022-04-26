f06186c9e423207101a4555a2ba3f29a7a8ce373
Molly Miller <55203910+mollym55@users.noreply.github.com>
2022-04-25 20:03:11 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,22 @@
 from resources import housing, metallic_alloys, electronics
 from transfer import Transfer
 from countries import Countries
-from time import sleep
 
 class Player:
 
     def __init__(self, name):
         self.name = name
         
+        
+class Game:
+    
+#Randomly chose my country to be Carpania
+myCountry = "Carpania"
+initialStatePath = "initial_countries.xlsx"
+resourcePath = "Sample-Resources.xlsx"
+transform_resources = [housing, metallic_alloys, electronics] 
+countries = Countries(myCountry, transform_resources,
+                          initialStatePath, resourcePath)   
+        
 
+p1 = Player("Player")
