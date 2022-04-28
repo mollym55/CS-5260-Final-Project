23ff6867ce52e6ef3769ecc44ec0d4ca16a6eb97
Molly Miller <55203910+mollym55@users.noreply.github.com>
2022-04-27 19:40:59 -0500

    Update game.py

--- game.py
+++ game.py
@@ -13,12 +13,18 @@ class Game:
     
     def play():
         #Randomly chose my country to be Carpania
-        myCountry = "Carpania"
+        #myCountry = "Carpania"
         initialStatePath = "initial_countries.xlsx"
         resourcePath = "Sample-Resources.xlsx"
         transform_resources = [housing, metallic_alloys, electronics] 
         countries = Countries(myCountry, transform_resources,
                                   initialStatePath, resourcePath)   
+        metallic_Elements = 0
+        timber = 0
+        water = 0
+        housing = 0
+        metallic_Alloys = 0
+        electronics = 0
 
         p1 = Player("Player")
         state = countries.getStateStatus()
@@ -27,6 +33,7 @@ class Game:
         #print(state)
         print("\nHello Player! This is the Beware of the Wumpus Game!")
         sleep(2)
+        print("\n")
         print("     ------------      ")
         print("    /  @    @    \     ")
         print("   /      v       \    ")
@@ -34,9 +41,49 @@ class Game:
         print(" /                  \  ")
         print(" \                  /  ")
         print("  \/\/\/\/\/\/\/\/\/   ")
+        print("\n")
         print("THE WUMPUS IS COMING FOR YOU!!!!")
         #print("\nYour country will be Carpania!")
         sleep(2)
+        print("\nWhat country do you choose?")
+        print("1. Atlantis")
+        print("2. Brobdingnag")
+        print("3. Carpania")
+        print("4. Dinotopia")
+        print("5. Erewhon")
+        choose_Country = input("")
+        if choose_Country == 1:
+            myCountry = "Atlantis"
+            print("\nYour country will be " + myCountry)
+            metallic_Elements = 700
+            timber = 2000
+            water = 50
+            housing = 100
+            metallic_Alloys = 200
+            electronics = 800
+            print("\nYour Resources (in millions):")
+            print("Metallic Elements -> " + metallic_Elements)
+            print("Timber -> " + timber)
+            print("Water -> " + water)
+            print("Housing -> " + housing)
+            print("Metallic Alloys -> " + metallic_Alloys)
+            print("Electronics -> " + electronics)
+        elif choose_Country == 2:
+            myCountry = "Brobdingnag"
+            print("\nYour country will be " + myCountry)
+        elif choose_Country == 3:
+            myCountry = "Carpania"
+            print("\nYour country will be " + myCountry)
+            
+        elif choose_Country == 4:
+            myCountry = "Dinotopia"
+            print("\nYour country will be " + myCountry)
+        elif choose_Country == 5:
+            myCountry = "Erewhon"
+            print("\nYour country will be " + myCountry)
+        else 
+            exit()
+        
         print("\nCarpania's Resources (in millions):")
         print("Metallic Elements -> 100")
         print("Timber -> 300")
@@ -52,3 +99,6 @@ class Game:
         play = input("")
         
     play()
+    
+    def exit():
+        print("\nBye! Hope you had fun :)")
